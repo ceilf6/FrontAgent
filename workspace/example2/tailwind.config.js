e9d4e1f9d1d2bc2caa403535e8ee0cc1848fd24f
WJH <3506456886@qq.com>
2025-12-28 16:00:44 +0800

    feat: 人工介入修复快照

---
+++ workspace/example2/tailwind.config.js
@@ -0,0 +1,41 @@
+var config = {
+    content: ['./index.html', './src/**/*.{ts,tsx}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
+export default config;
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+};
+base;
+components;
+utilities;
+base;
+{
+    body;
+    {
+        min - height;
+        100;
+        vh;
+        margin: 0;
+        font - family;
+        ui - sans - serif, system - ui, -apple - system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+            'Helvetica Neue', Arial, 'Noto Sans', 'Liberation Sans', sans - serif;
+        line - height;
+        1.5;
+        -webkit - font - smoothing;
+        antialiased;
+        -moz - osx - font - smoothing;
+        grayscale;
+        text - rendering;
+        optimizeLegibility;
+        color: rgb(17, 24, 39);
+        background - color;
+        rgb(255, 255, 255);
+    }
+}
